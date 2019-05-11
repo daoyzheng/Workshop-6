2d62fd985c65f20c2c28a73192870320b2faa466
Dao Zheng <daoyzheng@gmail.com>
2019-05-11 00:08:32 -0600

    Add select methods for supplier table,

--- src/DataAccessObjects/SuppliersDAOImpl.java
+++ src/DataAccessObjects/SuppliersDAOImpl.java
@@ -1,7 +1,12 @@
 package DataAccessObjects;
 
+import DataAccess.DbConnection;
 import DomainEntities.Supplier;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class SuppliersDAOImpl implements SuppliersDAO {
@@ -9,13 +14,53 @@ public class SuppliersDAOImpl implements SuppliersDAO {
     @Override
     public ArrayList<Supplier> getAllSuppliers() {
         ArrayList<Supplier> supplierArrayList = new ArrayList<>();
-
+        try {
+            // Get database connection
+            Connection conn = DbConnection.getConnection();
+            // Create query string
+            String query = "SELECT * FROM suppliers";
+            // Create prepare statement
+            PreparedStatement stmt = conn.prepareStatement(query);
+            // Execute Statement
+            ResultSet resultSet = stmt.executeQuery();
+            // Populate supplier array list
+            while (resultSet.next()) {
+                supplierArrayList.add(new Supplier(
+                        resultSet.getInt("SupplierId"),
+                        resultSet.getString("SupName")
+                ));
+            }
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return supplierArrayList;
     }
 
     @Override
     public Supplier getSupplierById(int supplierId) {
         Supplier supplier = null;
+
+        try {
+            // Get Db connection
+            Connection conn = DbConnection.getConnection();
+            // Create query string
+            String query = "SELECT * FROM suppliers WHERE SupplierId=?";
+            // Create prepare statement
+            PreparedStatement stmt = conn.prepareStatement(query);
+            // Assign parameter for prepare statement
+            stmt.setInt(1,supplierId);
+            // Execute statement
+            ResultSet resultSet = stmt.executeQuery();
+
+            supplier = new Supplier(
+                resultSet.getInt("SupplierId"),
+                resultSet.getString("SupName")
+            );
+
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
         return supplier;
     }
 
