139ff43bd95e22cb3b2175a761360f80416b16e0
Dao Zheng <daoyzheng@gmail.com>
2019-05-12 12:08:09 -0600

    Add productsupplier domain entity

---
+++ src/DomainEntities/ProductSupplier.java
@@ -0,0 +1,39 @@
+package DomainEntities;
+
+import javafx.beans.property.SimpleIntegerProperty;
+
+public class ProductSupplier {
+    private SimpleIntegerProperty productSupplierId;
+    private SimpleIntegerProperty productId;
+    private SimpleIntegerProperty supplierId;
+
+    public ProductSupplier(int productSupplierId, int productId, int supplierId) {
+        this.productSupplierId = new SimpleIntegerProperty(productSupplierId);
+        this.productId = new SimpleIntegerProperty(productId);
+        this.supplierId = new SimpleIntegerProperty(supplierId);
+    }
+
+    public int getProductSupplierId() {
+        return productSupplierId.getValue();
+    }
+
+    public void setProductSupplierId(int productSupplierId) {
+        this.productSupplierId.set(productSupplierId);
+    }
+
+    public int getProductId() {
+        return productId.getValue();
+    }
+
+    public void setProductId(int productId) {
+        this.productId.set(productId);
+    }
+
+    public int getSupplierId() {
+        return supplierId.getValue();
+    }
+
+    public void setSupplierId(int supplierId) {
+        this.supplierId.set(supplierId);
+    }
+}
