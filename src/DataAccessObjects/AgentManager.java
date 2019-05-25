d491275db0a77d6b822c7a7177210a800f0bb68e
peters2136 <peters2136@gmail.com>
2019-05-25 11:10:21 -0600

    Updated AgentManager

--- src/DataAccessObjects/AgentManager.java
+++ src/DataAccessObjects/AgentManager.java
@@ -2,6 +2,7 @@ package DataAccessObjects;
 
 import DataAccess.DbConnection;
 import DomainEntities.Agent;
+import javafx.scene.control.Alert;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -49,7 +50,7 @@ public class AgentManager {
 
     //retrieve a customer from database based on ID
     public static Agent getAgentById(int id) {
-        Agent a = new Agent();
+        Agent agt = null;
 
         try {
             //create DB connection
@@ -62,41 +63,123 @@ public class AgentManager {
 
             //create Array list to capture data from DB query rs
             while (rs.next()) {
-                a.setAgentId(rs.getInt(1));
-                a.setAgtFirstName(rs.getString(2));
-                a.setAgtMiddleInitial(rs.getString(3));
-                a.setAgtLastName(rs.getString(4));
-                a.setAgtBusPhone(rs.getString(5));
-                a.setAgtEmail(rs.getString(6));
-                a.setAgtPosition(rs.getString(7));
-                a.setAgtUserName(rs.getString(8));
-                a.setAgtPassword(rs.getString(9));
-                a.setAgencyId(rs.getInt(10));
+                agt = new Agent(
+                        Integer.parseInt(rs.getString(1)),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        rs.getString(6),
+                        rs.getString(7),
+                        rs.getString(8),
+                        rs.getString(9),
+                        Integer.parseInt(rs.getString(10)));
             }
+            System.out.println("closing conn");
             conn.close();
+
+
         }
-        catch (ClassNotFoundException | SQLException e) {
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("class not found exception");
+        }
+        catch (SQLException e) {
             e.printStackTrace();
         }
 
         finally {
-            return a;
+            return agt;
         }
-
     }
 
 
-    //add a new Agent to the database, returns true if successful
-    public static boolean addAgent(Agent agent) {
-        boolean result = true;
-
+    //retrieve a customer from database based on ID
+    public static int getAgentMaxId() {
+        int maxId = 0;
 
         try {
-            //create connection
+            //create DB connection
+            Connection conn =  DbConnection.getConnection();
+            //create statement
+            Statement stmt = conn.createStatement();
+
+            //query database
+            ResultSet rs = stmt.executeQuery("SELECT MAX(AgentId) FROM agents");
+
+            //create Array list to capture data from DB query rs
+            while (rs.next()) {
+                System.out.println(rs.getInt(1));
+            }
+            conn.close();
+
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("class not found exception");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return maxId;
+    }
+
+    //add a new Agent to the database, returns true if successful
+    public static int addAgent(Agent newAgent) {
+        int newAgtId = 0;
+
+        String sql = "INSERT INTO agents (" +
+                "AgtFirstName, " +
+                "AgtMiddleInitial, " +
+                "AgtLastName, " +
+                "AgtBusPhone, " +
+                "AgtEmail, " +
+                "AgtPosition, " +
+                "AgtUsername, " +
+                "AgtPassword, " +
+                "AgencyId) " +
+                "VALUES (?,?,?,?,?,?,?,?,?)";
+
+        System.out.println(sql);
+
+        try {
             Connection conn = DbConnection.getConnection();
+            PreparedStatement stmt = null;
+            stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1,newAgent.getAgtFirstName());
+            stmt.setString(2,newAgent.getAgtMiddleInitial());
+            stmt.setString(3,newAgent.getAgtLastName());
+            stmt.setString(4,newAgent.getAgtBusPhone());
+            stmt.setString(5,newAgent.getAgtEmail());
+            stmt.setString(6,newAgent.getAgtPosition());
+            stmt.setString(7,newAgent.getAgtUserName());
+            stmt.setString(8,newAgent.getAgtPassword());
+            stmt.setInt(9,newAgent.getAgencyId());
 
 
+            int numRows = stmt.executeUpdate();
+            if (numRows == 0)
+            {
+                System.out.println("add new agent not successful");
+                Alert alert = new Alert(Alert.AlertType.ERROR, "No rows were updated. Contact Tech Support.");
+                alert.showAndWait();
+            }
+            else {
+                System.out.println("add new agent successful");
+                //get id of new agent added to database
+                //create statement
+                Statement stmt2 = conn.createStatement();
 
+                //query database
+                ResultSet rs = stmt.executeQuery("SELECT MAX(AgentId) FROM agents");
+
+                while (rs.next()) {
+                    newAgtId = rs.getInt(1);
+                }
+            }
+            conn.close();
         }
         catch (ClassNotFoundException e) {
             e.printStackTrace();
@@ -104,18 +187,101 @@ public class AgentManager {
         catch (SQLException e) {
             e.printStackTrace();
         }
-        finally {
-            return result;
-        }
+        return newAgtId;
     }
 
 
     //update an Agent record in the database, returns true if successful
-    public static boolean updateAgent(Agent oldAgent, Agent newAgent){
+    public static boolean updateAgent(Agent changedAgent) throws SQLException, ClassNotFoundException {
+
+        String sql = "update Agents " +
+                "set AgtFirstName=?, " +
+                "AgtMiddleInitial=?, " +
+                "AgtLastName=?, " +
+                "AgtBusPhone=?, " +
+                "AgtEmail=?, " +
+                "AgtPosition=?, " +
+                "AgtUsername=?, " +
+                "AgtPassword=?, " +
+                "AgencyId=? " +
+                "where AgentId=?";
+
+        System.out.println(sql);
+        boolean result = false;
+        try {
+
+            Connection conn = DbConnection.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+
+            stmt.setString(1,changedAgent.getAgtFirstName());
+            stmt.setString(2,changedAgent.getAgtMiddleInitial());
+            stmt.setString(3,changedAgent.getAgtLastName());
+            stmt.setString(4,changedAgent.getAgtBusPhone());
+            stmt.setString(5,changedAgent.getAgtEmail());
+            stmt.setString(6,changedAgent.getAgtPosition());
+            stmt.setString(7,changedAgent.getAgtUserName());
+            stmt.setString(8,changedAgent.getAgtPassword());
+            stmt.setInt(9,changedAgent.getAgencyId());
+            stmt.setInt(10,changedAgent.getAgentId());
+
+            int numRows = stmt.executeUpdate();
+            if (numRows == 0)
+            {
+                Alert alert = new Alert(Alert.AlertType.ERROR, "No rows were updated. Contact Tech Support.");
+                alert.showAndWait();
+            }
+            else {
+
+                System.out.println("result = true");
+                result = true;
+            }
+
+            System.out.println("closing conn");
+
+            conn.close();
+
+            System.out.println("conn closed");
+        }
+        catch (SQLException e)
+        {
+            System.out.println("sql exception");
+            e.printStackTrace();
+        }
+        catch (ClassNotFoundException e)
+        {
+            System.out.println("class not found exception");
+            e.printStackTrace();
+        }
+        System.out.println("finally");
+        return result;
+    }
+
+    //check if a username is unqiue value
+    public static boolean checkAgentUsername(String username) {
         boolean result = true;
 
+        try {
+            //create DB connection
+            Connection conn =  DbConnection.getConnection();
+            //create statement
+            Statement stmt = conn.createStatement();
+
+            //query database
+            ResultSet rs = stmt.executeQuery("SELECT COUNT(AgtUserName) FROM agents WHERE AgtUserName=" + username);
 
 
+            //create Array list to capture data from DB query rs
+            System.out.println(rs.getFetchSize()+"");
+
+            conn.close();
+        }
+        catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("class not found exception");
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
         return result;
     }
 
